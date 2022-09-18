483bfa75992435da96733340907945f42e5d891e
JunghoJi <hodob76@gmail.com>
2022-09-18 18:29:09 +0900

    BoardRequestDto 생성
    게시판 요청을 담당

--- src/main/java/com/boardPractice/project/dto/board/BoardRequestDto.java
+++ src/main/java/com/boardPractice/project/dto/board/BoardRequestDto.java
@@ -1,5 +1,7 @@
 package com.boardPractice.project.dto.board;
 
+import com.boardPractice.project.entity.board.Board;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,5 +10,17 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class BoardRequestDto {
-
+	private Long id;
+	private String title;
+	private String content;
+	private String registerId;
+	
+	public Board toEntity() {
+		return Board.builder()
+				.title(title)
+				.content(content)
+				.registerId(registerId)
+				.build();
+	}
+	
 }
