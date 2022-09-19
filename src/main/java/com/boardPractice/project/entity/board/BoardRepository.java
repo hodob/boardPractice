a2e3da678aae6f2b65fc43ecd9517072ac825b62
hodob <hodob76@gmail.com>
2022-09-19 16:45:39 +0900

    boardService 추가

--- src/main/java/com/boardPractice/project/entity/board/BoardRepository.java
+++ src/main/java/com/boardPractice/project/entity/board/BoardRepository.java
@@ -14,10 +14,10 @@ public interface BoardRepository  extends JpaRepository<Board, Long>{
 	String UPDATE_BOARD = "UPDATE Board " +
 	"SET TITLE = :#{#boardRequestDto.title}, CONTENT = :#{#boardRequestDto.content}, UPDATE_TIME = NOW() WHERE ID = :#{#boardRequestDto.id}" ;
 	
-
 	@Transactional
 	@Modifying
 	@Query(value = UPDATE_BOARD, nativeQuery = true)
 	public int updateBoard(@Param("boardRequestDto") BoardRequestDto boardRequestDto);
+	
 	
 }
