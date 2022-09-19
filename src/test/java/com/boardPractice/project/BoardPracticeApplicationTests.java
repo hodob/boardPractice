d2a71c43c4811b021d57d9387c79a3770968eba7
hodob <hodob76@gmail.com>
2022-09-19 17:03:06 +0900

    중간저장 테스트 클래스 빌드중

--- src/test/java/com/boardPractice/project/BoardPracticeApplicationTests.java
+++ src/test/java/com/boardPractice/project/BoardPracticeApplicationTests.java
@@ -1,13 +1,36 @@
 package com.boardPractice.project;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.boardPractice.project.dto.board.BoardRequestDto;
+import com.boardPractice.project.service.board.BoardService;
 
 @SpringBootTest
 class BoardPracticeApplicationTests {
 
+	@Autowired
+	private BoardService boardService;
+	
+	
 	@Test
 	void contextLoads() {
+
 	}
+	void save() {
+		BoardRequestDto boardsaveDto = new BoardRequestDto();
+		
+		boardsaveDto.setTitle("제목입니다.");
+		boardsaveDto.setContent("내용입니다.");
+
+		
+		
+		
+		
+	}
+	
+	
+	
 
 }
